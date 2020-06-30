ddd3e55b272041b35bc913e9c1e2873563fa465a
Philip Cox <philcogm@gmail.com>
2020-06-30 23:51:04 +0000

    finished accounts pages and tied ID to posts made by account

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_30_222604) do
+ActiveRecord::Schema.define(version: 2020_06_30_223736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 2020_06_30_222604) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "account_id"
+    t.index ["account_id"], name: "index_posts_on_account_id"
     t.index ["category_id"], name: "index_posts_on_category_id"
   end
 
